5de16d13a334d28cb772476198abae182569e29d
Pranit Karale <pranit.karale2809@gmail.com>
2020-10-22 00:08:40 +0530

    An Example of Drawer Menu and Navigation through using keys.

---
+++ app/src/main/java/com/example/pranit/app1/ui/result/ResultFragment.java
@@ -0,0 +1,20 @@
+package com.example.pranit.app1.ui.result;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import com.example.pranit.app1.R;
+
+public class ResultFragment extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_result, container, false);
+    }
+}
